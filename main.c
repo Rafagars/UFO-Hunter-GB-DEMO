2e4711cea9950945b15f0cee44332c490b36d0cc
rafagars <rafagar96@gmail.com>
2021-02-28 00:25:13 -0400

    Added some background music

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include <gb/gb.h>
 #include <gb/font.h>
 #include <stdio.h>
+#include "gbt_player.h"
 #include "setups.h"
 #include "GameOverBackground.c"
 #include "PortadaGB_data.c"
 #include "PortadaGB_map.c"
+
+extern const unsigned char * song_Data[];
 
 void main(){
 
@@ -31,6 +34,9 @@ void main(){
 
     disable_interrupts();
 
+    gbt_play(song_Data, 2, 7);
+    gbt_loop(1);
+
     font_init();
     min_font = font_load(font_min); //36 tile
     font_set(min_font);
@@ -47,6 +53,7 @@ void main(){
     init();
 
     while(1){
+        wait_vbl_done();
         
         SHOW_WIN;
 
@@ -156,8 +163,8 @@ void main(){
                 setupbeam(0, 0); //That way we avoid that beam keep scrolling after disappearing from the screen
             }
 
-            wait_vbl_done();
+            
+            gbt_update(); //This will change to ROM bank 1
             }
     }
 }
-
--- run.sh
+++ run.sh
@@ -1,2 +1,10 @@
-/opt/gbdk/bin/lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c  main.o main.c
-/opt/gbdk/bin/lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -o  test.gb main.o
+./mod2gbt template.mod song 2
+
+/opt/gbdk/bin/lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o main.o main.c
+/opt/gbdk/bin/lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o output.o output.c
+/opt/gbdk/bin/lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o gbt_player.o gbt_player.s
+/opt/gbdk/bin/lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o gbt_player_bank1.o gbt_player_bank1.s 
+
+/opt/gbdk/bin/lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -Wl-yt1 -Wl-yo4 -Wl-ya0 -o test.gb main.o output.o gbt_player.o gbt_player_bank1.o
+
+rm *.o *.lst
