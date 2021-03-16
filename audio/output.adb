4ea2dfc75e68c00988c4c7285597f4112c7f3d1f
rafagars <rafagar96@gmail.com>
2021-03-16 11:42:24 -0400

    Fixed some errors on the bat file

--- audio/output.adb
+++
@@ -1,6 +0,0 @@
-M:output
-S:G$song0$0_0$0({322}DA322d,SC:U),D,0,0
-S:G$song1$0_0$0({382}DA382d,SC:U),D,0,0
-S:G$song2$0_0$0({379}DA379d,SC:U),D,0,0
-S:G$song3$0_0$0({368}DA368d,SC:U),D,0,0
-S:G$song_Data$0_0$0({10}DA5d,DG,SC:U),D,0,0
